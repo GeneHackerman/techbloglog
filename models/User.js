f4596797f4aa9038900b9de6761dbeb6e43df854
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-25 12:59:18 -0400

    spelling error

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequeliez');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
